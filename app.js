0145574283947ae1cf45c9e422089ec790a41350
antiloquax <tranter.m@sky.com>
2024-11-03 22:04:35 +0000

    Updated packages

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
- /*
-  * My YouTube mp3 downloader.
-  * Built on ytdl-core and ffmpeg.
-  */
+/*
+ * My YouTube mp3 downloader.
+ * Built on ytdl-core and ffmpeg.
+ */
 
-'use strict';
+"use strict";
 
 // Modules
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
-const EventEmitter = require('events');
-const {delFile, doUpdate, startProgress, logStr} = require('./js/helper');
-const {checkURL, fetchMp4} = require('./js/ytdl');
+const express = require("express");
+const path = require("path");
+const EventEmitter = require("events");
+const { delFile, doUpdate, startProgress } = require("./js/helper");
+const { checkURL, fetchMp4 } = require("./js/ytdl");
 
 // Set some variables
-dotenv.config();
 const port = process.env.PORT || 3000;
-const dir = path.join(__dirname, 'public');
-let mp3Emitter = new EventEmitter;
-let mp4Emitter = new EventEmitter;
-
-// MongoDB stuff
-const {MongoClient} = require('mongodb');
-const uri = process.env.SECRET;
-const client = new MongoClient(uri, 
-  {useNewUrlParser: true, useUnifiedTopology: true}
-);
-const db = client.db('ytdl');
-const coll = (port === 3000) ? 'downloads': 'onlineDownloads';
-client.connect((err) => {
-  if (err) { 
-    console.log('MongoDB failed to connect.');
-  }
-});
+const dir = path.join(__dirname, "public");
+let mp3Emitter = new EventEmitter();
+let mp4Emitter = new EventEmitter();
 
 // Start the server.
 const app = express();
@@ -46,20 +30,20 @@ app.use(express.json());
 
 // Routes
 // Try to get the video id and title and send to client.
-app.get('/video/', (req, res) => {
+app.get("/video/", (req, res) => {
   checkURL(res, req.query.url);
 });
 
 // Route for mp4 progress bar.
-app.get('/:id/mp4Event/', async (req, res) => {
+app.get("/:id/mp4Event/", async (req, res) => {
   function mp4Update(data) {
     doUpdate(res, data, req.params.id, mp4Emitter, mp4Update);
   }
-  startProgress(res, req.params.id, mp4Emitter, mp4Update); 
+  startProgress(res, req.params.id, mp4Emitter, mp4Update);
 });
 
 // Route for mp3 progress bar.
-app.get('/:id/mp3Event', async (req, res) => {
+app.get("/:id/mp3Event", async (req, res) => {
   function mp3Update(data) {
     doUpdate(res, data, req.params.id, mp3Emitter, mp3Update);
   }
@@ -67,56 +51,24 @@ app.get('/:id/mp3Event', async (req, res) => {
 });
 
 // This route starts the stream, extracts mp3 and adds the tags.
-app.post('/mp3', (req, res) => {
+app.post("/mp3", (req, res) => {
   fetchMp4(req.body.obj, mp4Emitter, mp3Emitter);
-  res.json({obj: req.body.obj});
+  res.json({ obj: req.body.obj });
 });
 
 // Route to provide download.
-app.post('/download', function(req, res) {
+app.post("/download", function (req, res) {
   const obj = req.body.obj;
   res.download(obj.audioFile);
-  // Update the database.
-  db.collection(coll).insertOne({text: logStr(obj.artist, obj.title)}, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
   // Clean up files.
-  setTimeout(function() {
-    delFile(path.join(__dirname, 'public', obj.thumb));
+  setTimeout(function () {
+    delFile(path.join(__dirname, "public", obj.thumb));
     delFile(path.join(__dirname, obj.audioFile));
     delFile(path.join(__dirname, obj.videoFile));
   }, 1000);
 });
 
-// Route for the log feature.
-app.post('/log', function(req, res) {
-  db.collection('users').findOne({name: req.body.user}, function(err, result) {
-    if (err) {
-      throw err;
-    } else {
-      if (!result) {
-        return res.status(401).send({error: 'User not found.'});
-      }
-      if (req.body.password !== result.password) {
-        return res.status(401).send({error: 'Incorrect password.'});
-      }
-      db.collection(coll).find().toArray((err, docs) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const msg = docs.reverse().map((elem) => elem.text).join('<br />');
-          res.json({log: msg});
-        }
-      });
-    }
-  });
-});
-
 // Anything else.
-app.all('*', function(req, res) {
-  res.status(404).send('Page not found.');
+app.all("*", function (_, res) {
+  res.status(404).send("Page not found.");
 });
-
-
--- public/myScripts.js
+++ public/myScripts.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /** Swap artist & title fields. */
 function swap() {
   let temp = formTitle.value;
@@ -8,50 +8,50 @@ function swap() {
 
 /** Pads a number with a leading 0. */
 function pad(str) {
-  return (`0${str}`).slice(-2);
+  return `0${str}`.slice(-2);
 }
 
 /** Human-friendly video length. */
 function lengthStr(s) {
-  let msg = "Video length: ";
+  let msg = 'Video length: ';
   let hours;
   let mins = Math.floor(s / 60);
-  let seconds = s - (mins * 60);
+  let seconds = s - mins * 60;
   if (mins > 59) {
     hours = Math.floor(mins / 60);
-    mins = mins - (hours * 60);
+    mins = mins - hours * 60;
     msg += `${hours}:${pad(mins)}:${pad(seconds)}`;
   } else {
     msg += `${pad(mins)}:${pad(seconds)}`;
   }
-  return `${msg}&nbsp;&nbsp;&nbsp;&nbsp`;
+  return `${msg}&nbsp;`;
 }
 
 /** Uses the video title to try to guess the tags. */
 function getTags(title, author) {
   var track;
   var artist;
-  var end = title.indexOf("[");
+  var end = title.indexOf('[');
   if (end == -1) {
-    end = title.indexOf("(Official");
+    end = title.indexOf('(Official');
   }
   if (end == -1) {
-    end = title.indexOf("(Lyric");
+    end = title.indexOf('(Lyric');
   }
   if (end != -1) {
     title = title.slice(0, end).trim();
   }
 
-  var ind = title.indexOf(" --");
+  var ind = title.indexOf(' --');
   if (ind == -1) {
-    ind = title.indexOf(" - ");
+    ind = title.indexOf(' - ');
   }
   if (ind != -1) {
     track = title.slice(ind + 3).trim();
     artist = title.slice(0, ind).trim();
   } else {
     track = title;
-    end = author.indexOf("- Topic");
+    end = author.indexOf('- Topic');
     if (end != -1) {
       author = author.slice(0, end).trim();
     }
@@ -59,5 +59,3 @@ function getTags(title, author) {
   }
   return { track: track, artist: artist };
 }
-
-
